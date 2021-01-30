cc01ba7521103f1afb83a40b594f0a214d43ee96
sgohl <8873017+sgohl@users.noreply.github.com>
2021-01-30 18:40:31 +0100

    Create swarm.sh

---
+++ parts/docker/swarm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+dnf -y install docker-ce
+systemctl enable --now docker
+
+case $HOST in
+  *1)
+    docker swarm init --advertise-addr 11.11.11.11
+    docker swarm join-token manager | grep join | tee /vagrant/.swarm-token-manager
+    ;;
+   *2|*3)
+   until [[ -f /vagrant/.swarm-token-manager ]] && docker ps; do sleep 1 ; done
+   bash /vagrant/.swarm-token-manager
+ esac
