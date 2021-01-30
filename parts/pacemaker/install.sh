5d6fe32e066c2b8fe29443d7775af46da1ff47cc
sgohl <8873017+sgohl@users.noreply.github.com>
2021-01-30 20:35:18 +0100

    Update install.sh

--- parts/pacemaker/install.sh
+++ parts/pacemaker/install.sh
@@ -11,6 +11,13 @@ totem {
     cluster_name: hacluster
     secauth: off
     transport: udpu
+
+    interface {
+      ringnumber: 0
+      bindnetaddr: 11.11.11.0
+      mcastport: 5405
+      broadcast: yes
+    }
 }
 
 quorum {
@@ -20,26 +27,27 @@ quorum {
 logging {
     to_logfile: yes
     logfile: /var/log/corosync.log
-    to_syslog: yes
 }
 
 nodelist {
+
+    node {
+        ring0_addr: centos1
+        nodeid: 1
+    }
+
+    node {
+        ring0_addr: centos2
+        nodeid: 2
+    }
+
+    node {
+        ring0_addr: centos3
+        nodeid: 3
+    }
+
+}
 EOF
-
-unset CONF
-for (( i=1; i <= ${NODES} ; i++ )); do
-CONF+="
-    node { 
-        ring0_addr: ${NAME}${i}
-        nodeid: ${i}
-    }    
-"
-done
-# tbd: generate with jo/jq
-
-echo "$CONF" | tee -a /etc/corosync/corosync.conf
-echo "}" | tee -a /etc/corosync/corosync.conf
-
 
 systemctl enable corosync --now
 systemctl enable pacemaker --now
@@ -49,13 +57,13 @@ systemctl restart corosync
 systemctl restart pacemaker
 systemctl restart pcsd
 
-case $HOSTNAME in 
+case $HOSTNAME in
   *3)
     sleep 10
 
     pcs property set stonith-enabled=false
     pcs property set no-quorum-policy=ignore
-    
+
     pcs resource create vip ocf:heartbeat:IPaddr2 ip=11.11.11.10 cidr_netmask=32 op monitor interval=1s
   ;;
 esac
